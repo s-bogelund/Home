41eb2c8af7bdf7517b54f86e908c232f3e771fdb
s-bogelund <simbj94@gmail.com>
2022-03-08 18:47:19 +0100

    added the ability to track site traffic with google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import ReactGA from "react-ga";
 import About from "./components/about/About";
 import Header from "./components/Header/Header";
 import ProductList from "./components/ProductList/ProductList";
 
-
 function App() {
-  return (
-    <Fragment>
-      <Header />
-      <About />
-      <ProductList/>
-    </Fragment>
-  );
+	useEffect(() => {
+		ReactGA.initialize("TRACKING ID");
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
+
+	return (
+		<Fragment>
+			<Header />
+			<About />
+			<ProductList />
+		</Fragment>
+	);
 }
 
 export default App;
